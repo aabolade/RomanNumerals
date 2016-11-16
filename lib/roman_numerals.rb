9c9fe51e9f84139b01cd234f9ed3eee47f33bfb9
Leke Abolade <leke_abolade92@hotmail.co.uk>
2016-11-16 09:08:50 +0000

    converts numbers 1, 5 and 10 into roman numerals

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,2 +1,12 @@
 class RomanNumerals
+
+  def convert(number)
+    return "X" if number % 10 == 0
+    return "V" if number % 5 == 0
+    "I"
+
+  end
+
+
+
 end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -1,4 +1,19 @@
 require 'roman_numerals'
 
 describe RomanNumerals do
+
+  subject(:roman_numerals) {described_class.new}
+
+  it "should convert the number 1 to I" do
+    expect(roman_numerals.convert(1)).to eq "I"
+  end
+
+  it "should convert the number 5 to V" do
+    expect(roman_numerals.convert(5)).to eq "V"
+  end
+
+  it "should convert the number 10 to X" do
+    expect(roman_numerals.convert(10)).to eq "X"
+  end
+
 end
